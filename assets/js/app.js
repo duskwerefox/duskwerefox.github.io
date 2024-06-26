e7e5a9d3c600be495894f9c1afafde473a61f5ff
user <user@debian>
2024-06-26 14:19:45 +0000

    test

---
+++ assets/js/app.js
@@ -0,0 +1,7 @@
+document.getElementById('send-btn').addEventListener('click', function() {
+    alert('Send button clicked');
+});
+
+document.getElementById('receive-btn').addEventListener('click', function() {
+    alert('Receive button clicked');
+});
